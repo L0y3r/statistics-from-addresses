72e077e6e4d798290a7d0109f85354832799a718
Marco Antonio Loyo <marcoantonioloyom@hotmail.com>
2020-05-26 00:06:18 -0500

    feat: Add code to load json files

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+'use strict';
+
 require('dotenv').config();
+const fs = require('fs');
 
 const genGoogleMapsURL = (address) =>
   `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
     address
   )}&key=${process.env.API_KEY}`;
+
+const ADDRESSES = JSON.parse(fs.readFileSync(process.env.JSON_FILE));
+
+const { nodes } = ADDRESSES;
+
+console.log(nodes);
