76c369db389f7053a2d05181d99ebffca890d90e
Marco Antonio Loyo <marcoantonioloyom@hotmail.com>
2020-05-26 02:27:02 -0500

    feat: Add function to create promises

--- index.js
+++ index.js
@@ -2,11 +2,25 @@
 
 require('dotenv').config();
 const fs = require('fs');
+const https = require('https');
 
 const genGoogleMapsURL = (address) =>
   `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
     address
   )}&key=${process.env.API_KEY}`;
+
+const createRequest = (address) =>
+  new Promise((resolve, reject) => {
+    https
+      .get(genGoogleMapsURL(address), (response) => {
+        let data = '';
+
+        response.on('data', (chunk) => (data += chunk));
+
+        response.on('end', () => resolve(JSON.parse(data).explanation));
+      })
+      .on('error', (error) => reject(error));
+  });
 
 const ADDRESSES = JSON.parse(fs.readFileSync(process.env.JSON_FILE));
 
@@ -19,7 +33,7 @@ const classifyData = nodes.reduce((dataObj, node) => {
   }
 
   node.attributes.location
-    ? dataObj[color].push(node.attributes.location)
+    ? dataObj[color].push(createRequest(node.attributes.location))
     : null;
 
   return dataObj;
